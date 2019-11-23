8671b2a7fb341beb4741950ce2a8271c1610363c
robin <skyfox@cloudendless.com>
2019-11-23 23:54:21 +0800

    1.update the version information.

--- TeconMoon's WiiVC Injector/Properties/AssemblyInfo.cs
+++ TeconMoon's WiiVC Injector/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TeconMoon's WiiVC Injector")]
+[assembly: AssemblyTitle("TeconMoon's WiiVC Injector Mod Version")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TeconMoon's WiiVC Injector")]
+[assembly: AssemblyProduct("TeconMoon's WiiVC Injector Mod Version")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
